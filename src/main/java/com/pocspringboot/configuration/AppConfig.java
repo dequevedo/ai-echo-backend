c5a416579fc0f5bb2c9590826d54d11f3bfb5a0e
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-02-16 23:58:50 -0300

    feat: Add AppConfig

---
+++ src/main/java/com/pocspringboot/configuration/AppConfig.java
@@ -0,0 +1,32 @@
+package com.pocspringboot.configuration;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import lombok.Getter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@Getter
+public class AppConfig {
+
+    @Value("${openai.api-key}")
+    private String openAIApiKey;
+
+    private final String speechToTextURL = "https://api.openai.com/v1/audio/transcriptions";
+
+    private final String textToSpeechURL = "https://api.openai.com/v1/audio/speech";
+
+    @Bean
+    public OpenAiChatModel openAiChatModel() {
+        return OpenAiChatModel.withApiKey(openAIApiKey).modelName();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
+
--- src/main/java/com/pocspringboot/service/ChatService.java
+++ src/main/java/com/pocspringboot/service/ChatService.java
@@ -1,11 +1,13 @@
 package com.pocspringboot.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pocspringboot.configuration.AppConfig;
 import com.pocspringboot.model.request.ChatRequest;
 import com.pocspringboot.model.request.TextToVoiceRequest;
 import com.pocspringboot.model.response.ChatResponse;
 import com.pocspringboot.model.response.VoiceToTextResponse;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -19,34 +21,30 @@ import java.util.Collections;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class ChatService {
 
     private final OpenAiChatModel chatModel;
-    private static final String SPEECH_TO_TEXT_URL = "https://api.openai.com/v1/audio/transcriptions";
-    private static final String TEXT_TO_SPEECH_URL = "https://api.openai.com/v1/audio/speech";
-    private String API_KEY; //TODO remover
+    private final RestTemplate restTemplate;
+    private final AppConfig appConfig;
 
-    private final ObjectMapper objectMapper = new ObjectMapper(); // Para converter o objeto para JSON
-
-
-    public ChatService(@Value("${openai.api-key}") String apiKey) {
-        API_KEY = apiKey;
-        this.chatModel = OpenAiChatModel.withApiKey(apiKey);
-    }
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public ChatResponse chat(ChatRequest request) {
-        log.info("Sending request: " + request);
-        var response = chatModel.generate(request.getMessage());
-        log.info("Received response: " + response);
-        return ChatResponse.builder().message(response).build();
+        try {
+            log.info("Sending request: " + request);
+            var response = chatModel.generate(request.getMessage());
+            log.info("Received response: " + response);
+            return ChatResponse.builder().message(response).build();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro enviar chat: " + e.getMessage());
+        }
     }
 
     public VoiceToTextResponse voiceToText(MultipartFile audioFile) {
         try {
-            RestTemplate restTemplate = new RestTemplate();
-
             HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(API_KEY);
+            headers.setBearerAuth(appConfig.getOpenAIApiKey());
             headers.setContentType(MediaType.MULTIPART_FORM_DATA);
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
@@ -56,7 +54,7 @@ public class ChatService {
 
             HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-            return restTemplate.exchange(SPEECH_TO_TEXT_URL, HttpMethod.POST, requestEntity, VoiceToTextResponse.class).getBody();
+            return restTemplate.exchange(appConfig.getSpeechToTextURL(), HttpMethod.POST, requestEntity, VoiceToTextResponse.class).getBody();
         } catch (Exception e) {
             throw new RuntimeException("Erro ao transcrever áudio: " + e.getMessage());
         }
@@ -64,24 +62,20 @@ public class ChatService {
 
     public ResponseEntity<byte[]> textToVoice(TextToVoiceRequest request) {
         try {
-            RestTemplate restTemplate = new RestTemplate();
-
             HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(API_KEY);
+            headers.setBearerAuth(appConfig.getOpenAIApiKey());
             headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM)); // Para receber o arquivo de áudio
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM));
 
-            // ✅ Serializa manualmente o objeto para JSON
             String requestBody = objectMapper.writeValueAsString(request);
 
             HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
 
-            // ✅ Envia a requisição e recebe o áudio como array de bytes
-            ResponseEntity<byte[]> response = restTemplate.exchange(TEXT_TO_SPEECH_URL, HttpMethod.POST, requestEntity, byte[].class);
+            ResponseEntity<byte[]> response = restTemplate.exchange(appConfig.getTextToSpeechURL(), HttpMethod.POST, requestEntity, byte[].class);
 
             return ResponseEntity.ok()
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"speech.mp3\"") // Nome do arquivo
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"speech.mp3\"")
                     .body(response.getBody());
 
         } catch (Exception e) {
